3d321d855d5741fe54a17343d901209b4118236d
Malachi <emalachithomas@gmail.com>
2026-03-30 08:18:41 -0700

    Created pydantic models to enforce correct inputs when user creates their profile. Added simple service logic to create the user profile (health_score logic will come later). Next is to add the new service function to the API endpoint

--- spotter/users/models.py
+++ spotter/users/models.py
@@ -1,4 +1,20 @@
 from pydantic import BaseModel
+from typing import List
+from enum import Enum
+
+class ExperienceLevel(Enum):
+    BEGINNER = 1
+    INTERMEDIATE = 2
+    ADVANCED = 3
+
+class MainGoal(Enum):
+    LoseWeight = 1
+    GainMuscle = 2
+    BuildStrength = 3
+    MaxVert = 4
+    MaxLongJump = 5
+    OverallHealth = 6
+
 
 class UserIntake(BaseModel):
     name: str
@@ -7,3 +23,12 @@ class UserIntake(BaseModel):
     height: float
     weight: float
     body_fat_percent: float
+    experience_level: ExperienceLevel
+    health_issues: List[str]
+    main_goal: MainGoal
+
+class UserProfile(BaseModel):
+    health_grade: float
+    intake_data: UserIntake
+
+
---
+++ spotter/users/services.py
@@ -0,0 +1,6 @@
+from spotter.users.models import UserIntake, UserProfile
+
+
+def profile_creation(profile_info: UserIntake) -> UserProfile:
+    new_user = UserProfile(health_grade= 0.0, intake_data=profile_info)
+    return new_user
