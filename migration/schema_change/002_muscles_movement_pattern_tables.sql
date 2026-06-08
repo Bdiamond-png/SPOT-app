97ab7ba079b92a6398b532bd484979ff4eb95379
Bdiamond-png <emalachithomas@gmail.com>
2026-06-08 11:37:38 -0700

    add migration 002: muscles, movement_pattern, exercise_muscles, user_goal_muscles tables

--- migration/schema_change/002_muscles_movement_pattern_tables.sql
+++ migration/schema_change/002_muscles_movement_pattern_tables.sql
@@ -19,3 +19,21 @@ CREATE TABLE exercise_muscles (
     FOREIGN KEY (muscles_id) REFERENCES muscles(id) ON DELETE CASCADE,
     FOREIGN KEY (movement_pattern_id) REFERENCES movement_pattern(id) ON DELETE CASCADE
 );
+CREATE TYPE public.muscles_type AS ENUM ('lagging', 'target');
+
+CREATE TABLE user_goal_muscles (
+    ID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    goals_id UUID,
+    muscles_id UUID,
+    muscles_type public.muscles_type,
+    FOREIGN KEY (muscles_id) REFERENCES muscles(id) ON DELETE CASCADE,
+    FOREIGN KEY (goals_id) REFERENCES user_goals(id) ON DELETE CASCADE
+);
+
+ALTER TABLE exercise DROP COLUMN movement_pattern;
+ALTER TABLE exercise DROP COLUMN primary_muscles;
+ALTER TABLE exercise DROP COLUMN assisting_muscles;
+ALTER TABLE user_goals DROP COLUMN target_muscles;
+ALTER TABLE user_goals DROP COLUMN lagging_muscles;
+ALTER TABLE exercise ADD COLUMN movement_pattern_id UUID REFERENCES movement_pattern(id) ON DELETE CASCADE;
+ALTER TABLE exercise ADD COLUMN exercise_muscles_id UUID REFERENCES exercise_muscles(id) ON DELETE CASCADE;
