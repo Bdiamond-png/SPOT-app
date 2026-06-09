16437100f5cf04e3b86ec756d59efcf3a1bb0aba
Bdiamond-png <emalachithomas@gmail.com>
2026-06-09 10:30:29 -0700

    finalize schema: clean exercise table, all dependent tables created

--- migration/schema_change/001_add_exercise_table.sql
+++ migration/schema_change/001_add_exercise_table.sql
@@ -1,21 +1,13 @@
-CREATE TYPE public.movementpattern AS ENUM ('horizontal_pull','vertical_pull',
-'horizontal_push', 'vertical_push','squat', 'hinge', 'unilateral', 'isometric',
-'transverse', 'plyometric');
-
-CREATE TYPE public.experiencelevel AS ENUM ('BEGINNER', 'INTERMEDIATE', 'ADVANCED');
-
 CREATE TABLE exercise (
     ID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(50) NOT NULL,
-    movement_pattern public.movement_pattern,
+    movement_pattern public.movementpattern,
     equipment public.workoutequipmentused,
     difficulty public.experiencelevel,
-    primary_muscle public.muscle_enum_type,
-    assisting_muscles public.muscle_enum_type[],
-    is_isolation BOOLEAN
+    is_isolation BOOLEAN,
 );
 
-ALTER TABLE exercise ADD COLUMN weight_score INT CHECK (weight_score BETWEEN 1 AND 100)
+ALTER TABLE exercise ADD COLUMN weight_score INT CHECK (weight_score BETWEEN 1 AND 100);
 ALTER TABLE exercise ADD COLUMN hypertrophy_score INT CHECK (hypertrophy_score BETWEEN 1 AND 10);
 ALTER TABLE exercise ADD COLUMN strength_score INT CHECK (strength_score BETWEEN 1 AND 10);
 ALTER TABLE exercise ADD COLUMN fatigue_cost INT CHECK (fatigue_cost BETWEEN 1 AND 10);
@@ -29,8 +21,8 @@ CREATE TYPE public.relationship_type AS ENUM ('progression', 'regression', 'equa
 
 CREATE TABLE exercise_relationships (
     ID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    exercise_id UUID REFERENCES exercise(id),
-    related_exercise_id UUID REFERENCES exercise(id),
+    exercise_id UUID REFERENCES exercise(id) ON DELETE CASCADE,
+    related_exercise_id UUID REFERENCES exercise(id) ON DELETE CASCADE,
     exercise_relationship_type public.relationship_type
 );
 
